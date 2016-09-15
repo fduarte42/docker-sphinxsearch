6b87390366995d267cd31af95ffd73f44f90a3d2
Tito Duarte <t.duarte@orangehive.de>
2016-09-15 11:53:54 +0200

    updated entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
-indexer --all
+if [ "$(ls -A /var/lib/sphinxsearch/data)" ]; then
+    echo "index already there, not indexing";
+else
+    echo "no index found, starting to index...";
+    indexer --all
+fi
 searchd --nodetach
